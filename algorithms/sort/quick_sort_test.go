79a0933375737495dfadbf9f3b2172ef2bc1894c
gzhh <zhonghuangao@gmail.com>
2022-05-14 17:35:09 +0800

    Add kmp algorithm

--- algorithms/sort/quick_sort_test.go
+++ algorithms/sort/quick_sort_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestQuickSort(t *testing.T)  {
+func TestQuickSort(t *testing.T) {
 	var nums = []int{4, 1, 8, 5, 3, 9, 2, 7, 3, 6}
 	var wantNums = []int{1, 2, 3, 3, 4, 5, 6, 7, 8, 9}
 	QuickSort(nums, 0, len(nums)-1)
